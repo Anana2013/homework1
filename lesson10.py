369e0e207c52ec3011d1dd767416a2e413ad4910
Anana2013 <rukhadzeana2013@gmail.com>
2025-08-13 19:55:56 +0400

    Add files via upload

---
+++ lesson10.py
@@ -0,0 +1,19 @@
+#For loop Python-ში განმეორებით აკეთებს ერთსა და იმავეს 
+for i in range(4,):
+    print(i)
+
+
+for i in range(5,):
+    print("სწავლა")
+
+    
+for i in range(9,):
+    print(i)
+
+
+for i in range(7,):
+    print("hello")
+
+
+for i in range(3,):
+    print("loop")
