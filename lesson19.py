fd5eac4c6465b9fce471ab36a5cfcccfed18f19c
Anana2013 <rukhadzeana2013@gmail.com>
2025-09-22 20:48:07 +0400

    Add files via upload

---
+++ lesson19.py
@@ -0,0 +1,35 @@
+nums = [10, 20, 30, 40, 50]
+print(nums[0])  
+print(nums[-1])  
+nums[1] = 200
+nums[2] = 999
+nums[-1] = 5000
+print(nums)
+letters = ["ა", "ბ", "გ", "დ", "ე"]
+print(letters[2])  
+print(letters[-1]) 
+letters[0] = "ზ"
+letters[2] = "თ"
+letters[-1] = "ი"
+print(letters)
+mix = ["apple", 25, True, 3.14]
+print(mix[0])  
+print(mix[-1])  
+mix[0] = "banana"
+mix[2] = False
+mix[1] = 100
+print(mix)
+words = ["კატა", "ძაღლი", "თაგვი", "კურდღელი", "ძროხა"]
+print(words[0])  
+print(words[2])  
+words[1] = "ვირი"
+words[2] = "ლომი"
+words[-1] = "ვეფხვი"
+print(words)
+nums2 = [5, 15, 25, 35, 45, 55]
+print(nums2[1])  
+print(nums2[4])  
+nums2[0] = 0
+nums2[2] = 333
+nums2[4] = 999
+print(nums2)
