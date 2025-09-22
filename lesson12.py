ae6588988ab31cc94fea4366e288045efe3d283a
Anana2013 <rukhadzeana2013@gmail.com>
2025-09-22 19:47:19 +0400

    Add files via upload

---
+++ lesson12.py
@@ -0,0 +1,25 @@
+i = 0
+while i < 20:
+    print(i)
+    i += 1
+
+
+i = 0
+while i <= 20:
+    if i == 2:
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+
+i = 10
+while i <= 50:
+    print(i)
+    i += 5
+
+
+i = 70
+while i >= 25:
+    print(i)
+    i -= 1
